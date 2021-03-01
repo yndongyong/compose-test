56a833734b556199f47f15ecfe022b8fc8f9592f
yndongyong <yndongyong@gmail.com>
2021-03-01 23:40:36 +0800

    first commit

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -15,47 +15,222 @@
  */
 package com.example.androiddevchallenge
 
+import android.media.Image
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.annotation.MainThread
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.Crossfade
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import com.example.androiddevchallenge.ui.theme.MyTheme
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private val screenViewModel by viewModels<ScreenViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MyTheme {
-                MyApp()
+        screenViewModel.screenData.observe(this){
+            setContent {
+                MyTheme {
+                    MyApp(screenViewModel)
+                }
             }
+        }
+    }
+
+    override fun onBackPressed() {
+        if (!screenViewModel.onBack()) {
+            super.onBackPressed()
         }
     }
 }
 
 // Start building your app here!
 @Composable
-fun MyApp() {
-    Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+fun MyApp(vm :ScreenViewModel) {
+    val curScreen = vm.screenData.observeAsState(true)
+
+    val dogList = arrayListOf<Dog>()
+    dogList.add(Dog("bronx1",1,R.drawable.bronx_1))
+    dogList.add(Dog("edison1",1,R.drawable.edison_1))
+    dogList.add(Dog("gunner1",1,R.drawable.gunner_1))
+    dogList.add(Dog("lucas1",1,R.drawable.lucas_1))
+
+    dogList.add(Dog("bronx2",1,R.drawable.bronx_1))
+    dogList.add(Dog("edison2",1,R.drawable.edison_1))
+    dogList.add(Dog("gunner2",1,R.drawable.gunner_1))
+    dogList.add(Dog("lucas2",1,R.drawable.lucas_1))
+
+    dogList.add(Dog("bronx3",1,R.drawable.bronx_1))
+    dogList.add(Dog("edison3",1,R.drawable.edison_1))
+    dogList.add(Dog("gunner3",1,R.drawable.gunner_1))
+    dogList.add(Dog("lucas3",1,R.drawable.lucas_1))
+
+    var position by remember { mutableStateOf(0) }
+
+    Crossfade(curScreen) {
+        Surface(color = MaterialTheme.colors.background) {
+
+            if (it.value) {
+                val state = rememberScrollState()
+                LaunchedEffect(Unit) { state.animateScrollTo(0) }
+                Column(
+                    Modifier
+                        .padding(16.dp)
+                        .verticalScroll(state))  {
+                    Text("Dog list",style = MaterialTheme.typography.h5)
+
+                    dogList.forEach {
+                        DogItem(dog = it,onClick = {
+                            position = dogList.indexOf(it)
+                            vm.navigateToDetail()
+                        },vm = vm)
+                    }
+                }
+            }else{
+                var curDog = dogList.get(position)
+                DetailScreen(modifier = Modifier, curDog){
+                    vm.navigateToHome()
+                }
+            }
+        }
+    }
+
+}
+
+
+class ScreenViewModel: ViewModel() {
+//    var screenData:LiveData<Boolean> = MutableLiveData(true)
+private val _screen = MutableLiveData<Boolean>(true)
+
+    var screenData: LiveData<Boolean> = _screen
+
+    @MainThread
+    fun onBack(): Boolean {
+        if (_screen.value !=true) {
+            _screen.value = false
+            return true
+        }
+        return false
+    }
+
+    @MainThread
+    fun navigateToDetail() {
+        _screen.value = false
+    }
+
+    @MainThread
+    fun navigateToHome() {
+        _screen.value = true
     }
 }
 
-@Preview("Light Theme", widthDp = 360, heightDp = 640)
 @Composable
-fun LightPreview() {
-    MyTheme {
-        MyApp()
+fun DetailScreen(modifier: Modifier = Modifier, dog: Dog, onClick: (Dog) -> Unit) {
+    val context = LocalContext.current
+    Column(modifier = Modifier.padding(16.dp)) {
+        AvatarHeaderImage(modifier = Modifier.clickable {
+            onClick.invoke(dog)
+        }, dog.avatarRes)
+        Spacer(modifier = Modifier.size(16.dp))
+        Button(onClick = {
+            Toast.makeText(context, "Belong to you!", Toast.LENGTH_LONG).show()
+            onClick.invoke(dog)
+        }) {
+            Text(text = "I want to adoption!")
+        }
+        Spacer(modifier = Modifier.size(16.dp))
+        Button(onClick = {
+            onClick.invoke(dog)
+        }) {
+            Text(text = "I want to see others")
+        }
     }
 }
+
+@Composable
+fun DogItem(modifier: Modifier = Modifier,dog: Dog,onClick: (Dog) -> Unit,vm :ScreenViewModel){
+    Row(
+        modifier = Modifier
+            .padding(16.dp)
+            .clickable {
+                onClick.invoke(dog)
+//                vm.screenData.value = false
+            }
+    ) {
+        AvatarImage(
+            Modifier.padding(end = 8.dp),
+            dog.avatarRes
+        )
+
+        Column(modifier = Modifier
+            .weight(1f)) {
+            Text("name ${dog.name}",style = MaterialTheme.typography.h5)
+            Text(text = "age ${dog.age}")
+        }
+    }
+}
+
+@Composable
+fun AvatarImage(modifier: Modifier = Modifier,avatarRes: Int) {
+    val image = ImageBitmap.imageResource(avatarRes)
+    Image(
+        bitmap = image,
+        contentScale = ContentScale.Crop,
+        contentDescription = null,
+        modifier = modifier
+            .size(120.dp, 80.dp)
+            .clip(MaterialTheme.shapes.medium)
+    )
+}
+
+@Composable
+fun AvatarHeaderImage(modifier: Modifier = Modifier,avatarRes: Int) {
+    val image = ImageBitmap.imageResource(avatarRes)
+    Image(
+        bitmap = image,
+        contentScale = ContentScale.Crop,
+        contentDescription = null,
+        modifier = modifier
+            .fillMaxWidth(1f)
+            .height(200.dp)
+            .clip(MaterialTheme.shapes.medium)
+    )
+}
+
+data class Dog(val name: String, val age: Int, val avatarRes: Int)
+
 
 @Preview("Dark Theme", widthDp = 360, heightDp = 640)
 @Composable
 fun DarkPreview() {
-    MyTheme(darkTheme = true) {
-        MyApp()
+
+    MyTheme(darkTheme = false) {
+        MyApp(ScreenViewModel())
     }
 }
